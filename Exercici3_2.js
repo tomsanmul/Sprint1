85892fd6bdcb8cf6e7cd1c410b192d937c032154
tomsanmul <tomsan@gmail.com>
2022-09-22 11:00:06 +0200

    Cambios

--- Exercici3_2.js
+++ Exercici3_2.js
@@ -1,7 +1,7 @@
 //Crea una funció anònima autoinvocable igualada a una variable que mostri per consola el nom de l'usuari/ària a rebut com a paràmetre.
-let Nom = "Tomas";
 
-let X = (function MostraNom(Nom) {
-    console.log(Nom);
-}
-);
+let Saluda = (function(nombre){ 
+    console.log("Hola "+nombre);
+})("Tomas");
+
+
