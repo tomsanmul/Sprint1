ebd0f7ecd3435c00b98713eec5080cc56c23c9c0
tomsanmul <tomsan@gmail.com>
2022-09-22 12:00:13 +0200

    Cambios

--- Exercici3_1.js
+++ Exercici3_1.js
@@ -2,27 +2,19 @@
 //Invoca cada funció de l'array iterativament. Haurà de mostrar-se per consola el compte del 0 al 9 deu vegades.
 
 
-    let array_de_funcions = [];
+let array_de_funcions = [];
 
-    for(let i=0;i <= 9; i++){
+for (let i = 0; i <= 9; i++) {
 
-        array_de_funcions.push(function() {(i)});
-        
-        
-        console.log(i);
-        
-    }
+    array_de_funcions.push(function () {
+        for (j = 0; j <= 9; j++) {
+            console.log("comptant..." + j);
+        }
+    })
 
-    console.log(array_de_funcions);
+}
 
-
-
-    /*
-    for(let j=0;j < array_de_funcions.length; j++){
-
-        array_de_funcions[j](funcio(j));
-        console.log(j);
-
-    }
-    */
-    
+for (let j = 0; j < array_de_funcions.length; j++) {
+    console.log("Funcio " + j);
+    array_de_funcions[j]();
+}
