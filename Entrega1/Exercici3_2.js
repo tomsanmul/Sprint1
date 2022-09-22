e7a9e249e593d7dd1db61b29a50c5f9ca12dac32
tomsanmul <tomsan@gmail.com>
2022-09-22 15:15:00 +0200

    Eliminar fitxers

--- Entrega1/Exercici3_2.js
+++
@@ -1,7 +0,0 @@
-//Crea una funció anònima autoinvocable igualada a una variable que mostri per consola el nom de l'usuari/ària a rebut com a paràmetre.
-
-let Nom = (function(nombre){ 
-    console.log(nombre);
-})("Tomas");
-
-
