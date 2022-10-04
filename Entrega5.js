b2b97d9c65b5e556d56ea3d245226e958b38ae2e
Tomas Sans <tomsan@gmail.com>
2022-10-04 13:11:00 +0200

    Canvis

--- Entrega5.js
+++ Entrega5.js
@@ -1,9 +1,32 @@
-//NIVELL 1
+//Nivell 1
 //Exercici 1
 //Crea una funció que, en executar-la, escrigui una frase en un fitxer.
+/*
 
 function EscriureFitxer(fitxer, frase) {
     let fs = require('fs');
+
+    fs.appendFile(fitxer, frase, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
+
+let fitxer = "archivo.txt";
+let frase = "Això es una prova" + "\r\n"; //Frase + salto de linea
+EscriureFitxer(fitxer, frase);
+
+
+//---------------------------------------------------------------------------------------
+//Nivell 1
+//Exercici 2
+//Crea una altra funció que mostri per consola el contingut del fitxer de l'exercici anterior.
+
+
+function LlegirFitxer(fitxer) {
+    let fs = require('fs');
+
     fs.readFile(fitxer, 'utf-8', (err, data) => {
         if (err) {
             console.log('error: ', err);
@@ -12,26 +35,41 @@ function EscriureFitxer(fitxer, frase) {
         }
     });
 
+}
 
-    fs.appendFile(fitxer, frase, function (err) {
-        // la funcion es la que maneja lo que sucede despues de termine el evento
-        if (err) {
-            return console.log(err);
-        }
+fitxer = "archivo.txt";
+LlegirFitxer(fitxer);
+
+
+//---------------------------------------------------------------------------------------
+//Nivell 1
+//Exercici 3
+//Crea una funció que comprimeixi el fitxer del nivell 1.
+
+function ComprimirFitxer() {
+    let cadena = LlegirFitxer(fitxer);
+    console.log(cadena);
+}
+
+ComprimirFitxer();
+*/
+
+//---------------------------------------------------------------------------------------
+//Nivell 1
+//Exercici 1
+//Crea una funció que imprimeixi recursivament un missatge per la consola amb demores d'un segon.
+
+
+
+const Imprimir_Missatge = async () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Això es un missatge amb demora de 1 segon")
+        }, 1000);
     });
 
-    /*
-    fs.writeFile(fitxer, frase, function (err) {
-        // la funcion es la que maneja lo que sucede despues de termine el evento
-        if (err) {
-            return console.log(err);
-        }
-    });
-    */
 }
 
 
-            let fitxer = "archivo.txt";
-            let frase = "Això es una prova2";
-
-            EscriureFitxer(fitxer, frase);
+let missatge = await Imprimir_Missatge;
+console.log(missatge);
