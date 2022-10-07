68194b6edbec41f902d82074c556f05469075fd5
tomsanmul <tomsan@gmail.com>
2022-10-07 15:52:15 +0200

    Canvis i arreglos

--- Entrega5.js
+++ Entrega5.js
@@ -12,67 +12,71 @@ function EscriureFitxer(fitxer, frase) {
     });
 }
 
-let fitxer = "archivo.txt";
-let frase = "Això es una prova" + "\r\n"; //Frase + salto de linea
+let fitxer = "fitxerInicial.txt";
+let frase = "Això es una prova d'escritura en un fitxer." + "\r\n"; //Frase + salto de linea
 EscriureFitxer(fitxer, frase);
-
+*/
 
 //---------------------------------------------------------------------------------------
 //Nivell 1
 //Exercici 2
 //Crea una altra funció que mostri per consola el contingut del fitxer de l'exercici anterior.
-
+/*
 function LlegirFitxer(fitxer) {
     let fs = require('fs');
     let texto = fs.readFileSync(fitxer, 'utf8');
     return (texto);
 }
 
-console.log(LlegirFitxer("archivo.txt"));
+console.log(LlegirFitxer("fitxerInicial.txt"));
+*/
 
 
 //---------------------------------------------------------------------------------------
 //Nivell 1
 //Exercici 3
 //Crea una funció que comprimeixi el fitxer del nivell 1.
-
+/*
 function ComprimirFitxer() {
     let AdmZip = require("adm-zip");
     let zip = new AdmZip();
-    let texto = LlegirFitxer("archivo.txt");
-    zip.addFile("archivo.txt", Buffer.from(texto, "utf8"), "");
-    zip.writeZip("archivo.zip");
+    zip.addLocalFile('fitxerInicial.txt');
+    zip.writeZip("fitxerInicial.zip");
+
 }
 
 ComprimirFitxer();
+*/
 
 
 //---------------------------------------------------------------------------------------
 //Nivell 2
 //Exercici 1
 //Crea una funció que imprimeixi recursivament un missatge per la consola amb demores d'un segon.
-
-function Repeteix_Missatge(){
+/*
+function Repeteix_Missatge() {
     setInterval(() => console.log('Això es una prova amb demora de 1 segon'), 1000);
 }
 
 Repeteix_Missatge();
-
+*/
 
 //-----------------------------------------------------------------------------------------
 //Nivell 2
 //Exercici 2
 //Crea una funció que llisti per la consola el contingut del directori d'usuari/ària de l'ordinador utilizant Node Child Processes.
-
-function LlistarArxius(){
+/*
+function LlistarArxius() {
     const fs = require('fs');
-    let arxius = fs.readdirSync("C:/Users/formacio");
-    for (let i=0; i<arxius.length;i++){
+    const os = require("os");
+    const userHomeDir = os.homedir();  //retorna la ruta (PATH) del directori Usuari
+    let arxius = fs.readdirSync(userHomeDir);
+    for (let i = 0; i < arxius.length; i++) {
         console.log(arxius[i]);
     }
 }
 LlistarArxius();
-
+*/
 
 //---------------------------------------------------------------------------------
 //Nivell 3
@@ -83,13 +87,13 @@ function EscriureDosFitxers() {
 
     let fs = require('fs');
     //Primer llegeixo el fitxer i el copio en una string amb codificació UTF8
-    let textoUTF8 = fs.readFileSync("archivo.txt", 'utf8');
+    let textoUTF8 = fs.readFileSync("fitxerInicial.txt", 'utf8');
 
     //Converteixo el textUTF8 en en Hexadecimal
     let stringHex = Buffer.from(textoUTF8, 'utf8').toString('hex');
 
     //Escric el fitxer en Hexadecimal
-    fs.writeFileSync("archivoHex.txt", stringHex, function (err) {
+    fs.writeFileSync("fitxerHex.txt", stringHex, function (err) {
         if (err) {
             return console.log(err);
         }
@@ -99,7 +103,7 @@ function EscriureDosFitxers() {
     let string64 = Buffer.from(textoUTF8, 'utf8').toString('base64');
 
     //Escric el fitxer en base64
-    fs.writeFileSync("archivo64.txt", string64, function (err) {
+    fs.writeFileSync("fitxer64.txt", string64, function (err) {
         if (err) {
             return console.log(err);
         }
@@ -108,19 +112,19 @@ function EscriureDosFitxers() {
 }
 
 EscriureDosFitxers();
-*/
+
 
 //------------------------------------------------------------------------------------------
 //Nivell 3
 //Exercici  2
 //Crea una funció que guardi els fitxers del punt anterior, 
 //ara encriptats amb l'algoritme aes-192-cbc, i esborri els fitxers inicials.
-
+/*
 function Encripta() {
 
     //Primer llegeixo el fitxer i el copio en una string amb codificació UTF8
     let fs = require('fs');
-    let textoUTF8 = fs.readFileSync("archivo.txt", 'utf8');
+    let textoUTF8 = fs.readFileSync("fitxerInicial.txt", 'utf8');
 
     //Instanciem crypto module
     let crypto = require('crypto');
@@ -134,31 +138,30 @@ function Encripta() {
 
     //Escric el nou fitxer encriptat.txt amb l'encriptació
 
-    fs.writeFileSync("encriptat.txt", strEncriptat, function (err) {
+    fs.writeFileSync("Encriptat.txt", strEncriptat, function (err) {
         if (err) {
             return console.log(err);
         }
     });
 
-/*
+
     //Esborro els altres 3 fitxers:  archivo.txt, archivoHex.txt i archivo64.txt 
     fs = require('fs').promises
     const files = [
-        './archivo.txt',
-        './archivoHex.txt',
-        './archivo64.txt',
+        './fitxerInicial.txt',
+        './fitxerHex.txt',
+        './fitxer64.txt',
     ]
     Promise.all(files.map(file => fs.unlink(file)))
         .then(() => {
-            console.log('"archivo.txt" , "archivoHex.txt" i "archivo64.txt" eliminats correctament.')
+            console.log('"fitxerInicial.txt" , "fitxerHex.txt" i "fitxer64.txt" eliminats correctament.')
         })
         .catch(err => {
-            console.error('Error al eliminar els fitxers "archivo.txt" , "archivoHex.txt" i "archivo64.txt"', err)
+            console.error('Error al eliminar els fitxers "fitxerInicial.txt" , "fitxerHex.txt" i "fitxer64.txt"', err)
         })
 }
-*/
-Encripta();
 
+Encripta();
 
 
 //Nivell 3
@@ -180,7 +183,14 @@ function DesEncripta() {
     let mystr = mykey.update(textoUTF8, 'hex', 'utf8')
     mystr += mykey.final('utf8');
 
-console.log(mystr); 
+    //Torno a escriure el fitxer inicial
+    fs.writeFile("fitxerInicial.txt", mystr, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+
 }
 
-console.log(DesEncripta());
+DesEncripta();
+*/
