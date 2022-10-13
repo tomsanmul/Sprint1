722c2d072c33d1d697b681ff9fb4a112a547ac6c
tomsanmul <tomsan@gmail.com>
2022-10-13 02:04:20 +0200

    Canvis

--- TDD/__tests__/funcions.test.js
+++ TDD/__tests__/funcions.test.js
@@ -1,62 +1,98 @@
-
-const { suma, resta, multiplica, divideix } = require('../app/funcions');
-
-//SUMA
-test('suma 2 + 2 es igual a 4', () => {
-  expect(suma(2, 2)).toBe(4);
-});
-
-test('Que els parámetres que li pasi no sigui buits', () => {
-  expect(suma("", "")).toBe(false);
-});
-
-test('Que els parámetres que li pasi no siguin text', () => {
-  expect(suma("hola", "th")).toBe(false);
-});
+const {
+  suma,
+  resta,
+  multiplica,
+  divideix,
+  getEmployee,
+  getSalary
+} = require('../app/funcions');
 
 
-//RESTA
-test('resta 2 - 1 es igual a 1', () => {
-  expect(resta(2, 1)).toBe(1);
-});
+describe('Operacions Suma, Resta, Multiplicar i Dividir', () => {
+  test('suma 2 + 2 es igual a 4', () => {
+    expect(suma(2, 2)).toBe(4);
+  });
 
-test('Que els parámetres que li pasi no sigui buits', () => {
-  expect(resta("", "")).toBe(false);
-});
+  test('resta 2 - 1 es igual a 1', () => {
+    expect(resta(2, 1)).toBe(1);
+  });
 
-test('Que els parámetres que li pasi no siguin text', () => {
-  expect(resta("hola", "th")).toBe(false);
-});
+  test('multiplica 2 per 3 es igual a 6', () => {
+    expect(multiplica(2, 3)).toBe(6);
+  });
 
+  test('divideix 4 entre 2 es igual a 2', () => {
+    expect(divideix(4, 2)).toBe(2);
+  });
 
-//MULTIPLICA
-test('multiplica 2 per 3 es igual a 6', () => {
-  expect(multiplica(2, 3)).toBe(6);
-});
-
-test('Que els parámetres que li pasi no sigui buits', () => {
-  expect(multiplica("", "")).toBe(false);
-});
-
-test('Que els parámetres que li pasi no siguin text', () => {
-  expect(multiplica("hola", "th")).toBe(false);
 });
 
 
-//DIVIDEIX
-test('divideix 4 entre 2 es igual a 2', () => {
-  expect(divideix(4, 2)).toBe(2);
+describe('Revisió de paràmetres', () => {
+
+  test('Que els parámetres que rebi no sigui buit', () => {
+    expect(suma("", "")).toBe(false);
+  });
+
+  test('Que els parámetres que li pasi no siguin text', () => {
+    expect(suma("hola", "th")).toBe(false);
+  });
+
+  test('Que els parámetres que rebi no sigui buit', () => {
+    expect(resta("", "")).toBe(false);
+  });
+
+  test('Que els parámetres que li pasi no siguin text', () => {
+    expect(resta("hola", "th")).toBe(false);
+  });
+
+  test('Que els parámetres que rebi no sigui buit', () => {
+    expect(multiplica("", "")).toBe(false);
+  });
+
+  test('Que els parámetres que li pasi no siguin text', () => {
+    expect(multiplica("hola", "th")).toBe(false);
+  });
+
+  test('Que els parámetres que rebi no sigui buit', () => {
+    expect(divideix("", "")).toBe(false);
+  });
+
+  test('Que els parámetres que li pasi no siguin text', () => {
+    expect(divideix("hola", "th")).toBe(false);
+  });
+
 });
-
-test('Que els parámetres que li pasi no sigui buits', () => {
-  expect(divideix("", "")).toBe(false);
-});
-
-test('Que els parámetres que li pasi no siguin text', () => {
-  expect(divideix("hola", "th")).toBe(false);
-});
-
-
 
 
 //Crea els tests corresponents per verificar el funcionament de les dues funcions de l'exercici Promises i Callbacks N1 E2.
+
+//Crea els tests corresponents per verificar el funcionament de les funcions de l'exercici 
+//Promises i Callbacks N2 E1 i Promises i Callbacks N2 E2 (getEmployee() i getSalary()).
+describe('Verificar funcionament de Exercici Promises i Callbacks', () => {
+
+  test('El Id 1 debe coinicidir con Linux Torvals', () => {
+    const obj = {
+      id: '1',
+      name: 'Linux Torvalds'
+    };
+
+    expect(getEmployee(1));
+    expect(obj.id).toEqual('1');
+    expect(obj.name).toEqual('Linux Torvalds');
+
+  });
+
+
+/*
+  test('El Id 1 debe coinicidir con Linux Torvals', () => {
+    const obj = {
+      id: 1,
+      name: 'Linux Torvalds'
+    };
+    expect(getSalary(obj)).toBe('4000');
+
+  });
+*/
+
+});
